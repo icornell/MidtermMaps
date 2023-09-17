deb0683deef244de948d452b6acf7a2bc821630f
lunamoonmoon <5862lunamoon@gmail.com>
2023-09-17 10:08:56 -0700

    add seeds

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -2,6 +2,6 @@
 
 INSERT INTO maps (id, user_key, name, date_created)
 VALUES
-(),
-(),
-();
+('1', '2', 'Vacation Plans', '2023-08-16'),
+('2', '1', 'Best Restaurants', '2021-10-06'),
+('3', '3', 'Places to Visit', '2023-06-30');
--- db/seeds/03_userfavs.sql
+++ db/seeds/03_userfavs.sql
@@ -2,6 +2,6 @@
 
 INSERT INTO user_favourites (id, user_key, maps_key)
 VALUES
-(),
-(),
-();
+('1', '1', '3'),
+('1', '2', '3'),
+('1', '3', '1');
